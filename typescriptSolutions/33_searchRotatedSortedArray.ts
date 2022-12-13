576cdc933ef002e5c92a1df889b6958b5d6faa5f
dariocar1990 <dario.car.kz@gmail.com>
2022-12-13 10:31:57 +0100

    33 done

--- typescriptSolutions/33_searchRotatedSortedArray.ts
+++ typescriptSolutions/33_searchRotatedSortedArray.ts
@@ -6,3 +6,37 @@ For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6
 Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
 You must write an algorithm with O(log n) runtime complexity.
 */
+
+const search = (nums: number[], target: number): number => {
+  // The 'left' and 'right' variables are used to keep track of the position of the search area.
+  let left = 0;
+  let right = nums.length - 1;
+
+  // The 'mid' variable is used to store the midpoint of the search area.
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    if (nums[mid] === target) {
+      return mid;
+    }
+
+    // Check if the target is at the midpoint position
+    if (nums[left] <= nums[mid]) {
+      // Check if the target is within the left side of the midpoint.
+      if (nums[left] <= target && target <= nums[mid]) {
+        right = mid - 1;
+      } else {
+        left = mid + 1;
+      }
+      // Check if the target is within the right side of the midpoint.
+    } else {
+      if (nums[mid] <= target && target <= nums[right]) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+    }
+  }
+
+  // return -1 if the target is not found
+  return -1;
+};
